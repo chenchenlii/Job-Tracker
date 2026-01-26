f21cf55bca7e0c47527f17af2846ba738ed4d961
Yangchenchen <yangchenchen.li@uq.net.au>
2026-01-26 16:52:33 +1000

    setup gitignore

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -71,11 +71,14 @@ WSGI_APPLICATION = 'job_tracker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project_db',       # Your database name
+        'USER': 'project_user',       # Your PostgreSQL username
+        'PASSWORD': 'Password!',   # Your PostgreSQL password
+        'HOST': 'localhost',           # Usually 'localhost' for local development
+        'PORT': '5432',                # Default PostgreSQL port
     }
 }
 
