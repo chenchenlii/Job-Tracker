129f338d36f04a5cad1163994d42a149a821066c
Yangchenchen <yangchenchen.li@uq.net.au>
2026-01-26 17:16:41 +1000

    setup env, db

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -9,21 +9,34 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import environ
+import os # Import os for path joining
 from pathlib import Path
+
+# Initialise environment variables
+env = environ.Env(
+    # Set casting, default value
+    DEBUG=(bool, False) 
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Read .env file
+# This needs to be after BASE_DIR is defined
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'), overwrite=True)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b*b@o8(08pou6s8s9$&149)tnm_73$@24mnl@b#k8o)xpu(3(b'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -73,12 +86,10 @@ WSGI_APPLICATION = 'job_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project_db',       # Your database name
-        'USER': 'project_user',       # Your PostgreSQL username
-        'PASSWORD': 'Password!',   # Your PostgreSQL password
-        'HOST': 'localhost',           # Usually 'localhost' for local development
-        'PORT': '5432',                # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
     }
 }
 
