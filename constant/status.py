369757973469eb1b674fec8d36bb9bd4116d5536
Yangchenchen <yangchenchen.li@uq.net.au>
2026-02-02 17:02:27 +1000

    created the job application status choices

---
+++ constant/status.py
@@ -0,0 +1,8 @@
+APPLICATION_STATUS = (
+    ("saved", "Saved"),
+    ("applied", "Applied"),
+    ("interview", "Interview"),
+    ("offer", "Offer"),
+    ("rejected", "Rejected")
+    ("ghosted", "Ghosted")
+)
