38411d6241a032a44a1ea42d455cc68a2d21f372
Yangchenchen <yangchenchen.li@uq.net.au>
2026-02-03 19:55:55 +1000

    register the admin

--- applications/admin.py
+++ applications/admin.py
@@ -4,7 +4,20 @@ from .models import JobApplication
 
 
 class JobApplicationAdmin(admin.ModelAdmin):
-    list_display = ("user_id", "job_title", "company_name", "platform", "applied_at")
+    list_display = ("user", "job_title", "company_name", "platform", "applied_at")
+    raw_id_fields = ("user",)
+    list_filter = (
+        "status",
+        "platform",
+        "applied_at",
+        "created_at",
+    )
+    search_fields = (
+        "job_title",
+        "company_name",
+        "platform",
+        "user__email",
+    )
 
 
 admin.site.register(JobApplication, JobApplicationAdmin)
