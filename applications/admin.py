57a939bf38165eaa233b2d54582a08cfa49d0eda
Chenchen <114964359+chenchenlii@users.noreply.github.com>
2026-02-04 20:33:18 +1000

    Create application status and interview models (#5)
    * created application status history and interview models
    
    * updated field name
    
    * admin and models done

--- applications/admin.py
+++ applications/admin.py
@@ -1,6 +1,77 @@
 from django.contrib import admin
 
-from .models import JobApplication
+from .models import ApplicationStatusHistory, JobApplication, Interview
+
+
+
+class ApplicationStatusHistoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "application",
+        "from_status",
+        "to_status",
+        "changed_at",
+    )
+
+    list_filter = (
+        "application",
+        "from_status",
+        "to_status",
+    )
+
+    search_fields = (
+        "application__job_title",
+        "application__company_name",
+        "changed_by__email",
+    )
+
+    ordering = ("-changed_at",)
+
+
+class ApplicationStatusHistoryInline(admin.TabularInline):
+    model = ApplicationStatusHistory
+    extra = 0
+    can_delete = False
+    readonly_fields = (
+        "from_status",
+        "to_status",
+        "changed_by",
+        "changed_at",
+    )
+
+
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "application",
+        "round",
+        "interview_type",
+        "interview_date",
+        "interview_status",
+    )
+
+    list_filter = (
+        "application",
+        "interview_type",
+        "interview_status",
+        "interview_date",
+    )
+
+    search_fields = (
+        "application__job_title",
+        "application__company_name",
+        "interviewer",
+    )
+
+
+class InterviewInline(admin.TabularInline):
+    model = Interview
+    extra = 0
+    fields = (
+        "round",
+        "interview_type",
+        "interview_date",
+        "interview_status",
+    )
+    ordering = ("round",)
 
 
 class JobApplicationAdmin(admin.ModelAdmin):
@@ -18,6 +89,13 @@ class JobApplicationAdmin(admin.ModelAdmin):
         "platform",
         "user__email",
     )
+    inlines = (
+        ApplicationStatusHistoryInline,
+        InterviewInline,
+    )
 
 
+admin.site.register(ApplicationStatusHistory, ApplicationStatusHistoryAdmin)
+admin.site.register(Interview, InterviewAdmin)
 admin.site.register(JobApplication, JobApplicationAdmin)
+
