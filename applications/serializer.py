7cab04c082b32889fef28f428febabf19f5199fc
Yangchenchen <yangchenchen.li@uq.net.au>
2026-02-09 16:11:18 +1000

    add serializer

---
+++ applications/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import JobApplication
+
+
+class JobApplicationStatusUpdateSerializer(serializers.Serializer):
+    status = serializers.ChoiceField(
+        choices=JobApplication.Status.choices
+    )
