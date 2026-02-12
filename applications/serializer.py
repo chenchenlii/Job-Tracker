163fe64818819426c21995bf184149a43c87ca9d
Yangchenchen <yangchenchen.li@uq.net.au>
2026-02-12 14:11:06 +1000

    black

--- applications/serializer.py
+++ applications/serializer.py
@@ -3,6 +3,4 @@ from .models import JobApplication
 
 
 class JobApplicationStatusUpdateSerializer(serializers.Serializer):
-    status = serializers.ChoiceField(
-        choices=JobApplication.Status.choices
-    )
+    status = serializers.ChoiceField(choices=JobApplication.Status.choices)
